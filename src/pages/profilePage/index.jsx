9462e262d24718efb860506b3cc87bde12f5f8e2
Sindhuja Reddy Kamidi <sindhuja@Sindhujas-MacBook-Pro.local>
2023-03-12 13:07:04 -0700

    MyMemorygram client code

---
+++ src/pages/profilePage/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import NavigationBar from "pages/navigationBar";
+
+const ProfilePage = () => {
+    return (
+        <Box>
+            <NavigationBar/>
+        </Box>
+    );
+}
+
+export default ProfilePage;
